a510d1f1e5b916ea2fde6353584602061ff01a77
Ricard Roberg <ricardroberg@gmail.com>
2024-11-21 10:00:24 -0300

    removendo desafios da aula3

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -3,7 +3,9 @@ import { conectarAoBanco } from "../config/dbConfig.js";
 export const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 export async function getTodosOsPosts() {
+  console.log("1");
   const db = conexao.db("imersao-instabytes");
+  console.log("2");
   const colecao = db.collection("posts");
   return colecao.find().toArray();
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { listarPosts } from "../controllers/postsController.js";
 
-export const postRoutes = (app) => {
+export const routes = (app) => {
   app.use(express.json());
+  
   app.get("/posts", listarPosts);
 };
 
