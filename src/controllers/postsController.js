5ecc33ebcf690f647a1c713149a8cd2d6a471665
Ricard Roberg <ricardroberg@gmail.com>
2024-11-24 22:36:35 -0300

    finalizada aula 05

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,10 @@
-import { getTodosOsPosts, criarPost } from "../models/postModel.js";
 import fs from "fs";
+import gerarDescricaoComGemini from "../services/geminiService.js";
+import {
+  getTodosOsPosts,
+  criarPost,
+  atualizarPost,
+} from "../models/postModel.js";
 
 export async function listarPosts(req, resp) {
   const posts = await getTodosOsPosts();
@@ -23,14 +28,35 @@ export async function uploadImagem(req, resp) {
     imgUrl: req.file.originalname,
     alt: "",
   };
-  
+
   try {
     const postCriado = await criarPost(novoPost);
-    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
-    fs.renameSync(req.file.path, imagemAtualizada)
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+    fs.renameSync(req.file.path, imagemAtualizada);
     resp.status(200).json(postCriado);
   } catch (error) {
     console.error(error.message);
     resp.status(500).json({ Erro: "Não foi possível enviar imagem!" });
+  }
+}
+
+export async function atualizarNovoPost(req, res) {
+  const id = req.params.id;
+  const urlImagem = `http://localhost:3000/${id}.png`;
+  try {
+    const imgBuffer = fs.readFileSync(`uploads/${id}.png`);
+    const descricao = await gerarDescricaoComGemini(imgBuffer);
+
+    const post = {
+      imgUrl: urlImagem,
+      descricao: descricao,
+      alt: req.body.alt,
+    };
+
+    const postCriado = await atualizarPost(id, post);
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ Erro: "Falha na requisição" });
   }
 }
